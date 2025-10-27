35682d26688589c2ad0685ed02bb5aac98215525
Emma <emmanuelibbk@gmail.com>
2025-10-27 01:38:17 +0100

    Ss

--- app/instagram/router.py
+++ app/instagram/router.py
@@ -899,10 +899,11 @@ async def instagram_auth_login(
         
         # Scopes needed for messaging and account info
         scopes = [
-            "instagram_basic",
-            "instagram_manage_messages",
             "pages_show_list",
-            "pages_manage_metadata"
+            "pages_read_engagement",
+            "pages_manage_metadata",
+            "instagram_basic",
+            "instagram_manage_messages"
         ]
         
         # Pass tenant_id in state to identify user on callback
